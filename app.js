5acc3d5b355bb6b037d527501add6f08690eb644
Keith Levine <Levine.Keith@gmail.com>
2021-08-09 19:17:03 -0700

    updated naming convention, and added comments

--- app.js
+++ app.js
@@ -2,42 +2,44 @@
 const submitButton = document.querySelector('#submitBtn')
 const todoContainer = document.querySelector('.todo')
 const completedDiv = document.querySelector('.completed')
-
-// Create Elements
-const removeBtn = document.createElement('button')
-let listItemDiv = document.createElement('div')
-let completeBtn = document.createElement('button')
-
-const todoArray = []
+const thingsToDo = []
 
 // DOM manipulation to render items
 const renderList = () => {
+    const toDoItem = document.createElement('div')
+    const completeBtn = document.createElement('button')
     completeBtn.innerText = 'Completed'
     completeBtn.setAttribute('id', 'lists')
 
-    todoArray.forEach((item) => {
-        listItemDiv.innerText = item
-        listItemDiv.className = 'todo-item'
-        todoContainer.append(listItemDiv)
-        listItemDiv.append(completeBtn)
+    // displays inner tect from form and adds class
+    thingsToDo.forEach((item) => {
+        toDoItem.innerText = item
+        toDoItem.className = 'todo-item'
+        todoContainer.append(toDoItem)
+        toDoItem.append(completeBtn)
     })
-}
 
-completeBtn.onclick = () => {
-    completedDiv.append(listItemDiv)
-    listItemDiv.className = 'done-item'
-    completeBtn.remove()
-    removeBtn.innerText = 'Remove'
+    // Move toDo item to completed
+    completeBtn.onclick = () => {
+        completedDiv.append(toDoItem)
+        toDoItem.className = 'done-item'
+        completeBtn.remove()
+        
+        const removeBtn = document.createElement('button')
 
-    listItemDiv.append(removeBtn)
-    removeBtn.onclick = () => {
-        listItemDiv.remove()
+        removeBtn.innerText = 'Remove'
+        toDoItem.append(removeBtn)
+
+        removeBtn.onclick = () => {
+        toDoItem.remove()
+        }
     }
 }
 
+// onclick function to render the list
 submitButton.onclick = () => {
     const inputValue = document.querySelector('#input-box').value
-    todoArray.push(inputValue)
-    console.log(todoArray)
+    thingsToDo.push(inputValue)
+    document.querySelector('#input-box').value = ''
     renderList()
 }
